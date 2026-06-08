28974cb0905bc15c411f08a3f1c544ddd2372f80
Abhishek <abhishkumar159@gmail.com>
2026-06-08 17:23:43 +0530

    all setup configured

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -75,7 +75,7 @@ UserSchema.methods.generateRefreshToken = function(){
    },
    process.env.REFRESH_TOKEN_SECRET,
    {
-    expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY 
    }
 )
 }
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,20 +1,20 @@
 const asyncHandler = (requestHandler) => {
-    (req, res, next) =>{
-        Promise.resolve(requestHandler(req,res,next)).
-        catch((err)=>next(err))
+   return (req, res, next) =>{
+        Promise.resolve(requestHandler(req,res,next))
+        .catch((err)=>next(err))
     }
 }
 
- export default asyncHandler
+ export {asyncHandler}
 
 
- const asyncHandler = (fn) => async(req, res, next) =>  {
-    try{
-     await fn(req, res, next)
-    }catch(error){
-        res.status(error.code || 500).json({
-            success: false,
-            message: error.message
-        })
-    }
- }
+//  const asyncHandler = (fn) => async(req, res, next) =>  {
+//     try{
+//      await fn(req, res, next)
+//     }catch(error){
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+//  }
