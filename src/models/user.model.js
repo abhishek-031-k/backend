760928f13605c612496e13cd77acbc8932aa2644
Abhishek <abhishkumar159@gmail.com>
2026-06-12 18:18:25 +0530

    uer register

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -41,13 +41,13 @@ const UserSchema = new mongoose.Schema({
       required: [true, 'Password is required']
      },
      refreshToken: {
-        type: string
+        type: String
      }
 }, {timestamps: true})
 
 UserSchema.pre("save", async function (next) {
    if(!this.isModified("password"))return next();
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
