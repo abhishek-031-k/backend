3b3b9a9f6ef2f7967d5006393c0bb4460cfce8e6
Abhishek <abhishkumar159@gmail.com>
2026-06-18 19:27:04 +0530

    fix: correct function name and enhance getUserChannelProfile with subscription details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -285,18 +285,69 @@ res
 
 })
 
-const getUserChannerlProfile = asyncHandler(async(req, res)=>{
+const getUserChannelProfile = asyncHandler(async(req, res)=>{
  const {useranme} = req.params
  if(!username?.trim())throw new ApiError(400, "username is missing")
 
-  const Channnel = await User.aggregate([
+  const Channel = await User.aggregate([
     {
       $match: {
         username: username?.toLowerCase()
       }
-    }
+    },
+    {
+      $lookup:{
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers"
+      }
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo"
+      }
+    },
+      {
+        $addFields: {
+          subscribersCount: {
+            $size: "$subscribers"
+          },
+          channelssubscribedToCount: {
+            $size: "$subscribedTo"
+          },
+          issubscribed:{
+            $cond: {
+              if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+              then: true,
+              else: false
+            }
+          }
+        }
+      },
+      {
+        $project: {
+          fullName: 1,
+          username: 1,
+          subscribersCount: 1,
+          channelssubscribedToCount: 1,
+          isissubscribed: 1,
+          avatar: 1,
+          covverImage: 1,
+          email: 1
+        }
+      }
   ])
+
+  if(!Channel?.length)throw new ApiError(404, "channel does not exists")
+  return res
+.status(200)
+.json(new ApiResponse(200, channel[0], "user channel fetched successfully"))
 })
+
 
 export {
   registerUser,
@@ -305,8 +356,8 @@ export {
     refreshaccessToken,
      changeCurrentPassword,
       getCurrentUser,
-      updateAccountDetails,
+       updateAccountDetails,
       updateUserAvatar,
       updateUserCoverImage,
-      getUserChannerlProfile
+      getUserChannelProfile
     }
