c8e30c09f0de3a849d18142678b02ceb04bdb779
Abhishek <abhishkumar159@gmail.com>
2026-06-29 00:02:06 +0530

    fix: update comment function documentation and add error handling for invalid comment ID

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -32,7 +32,7 @@ const addComment = asyncHandler(async (req, res) => {
 })
 
 const updateComment = asyncHandler(async (req, res) => {
-    // update comment
+    // update comments
     const {commentId} = req.params
     const {content} = req.body
     if(!mongoose.Types.ObjectId.isValid(commentId))throw new ApiError(400, "invalid Id")
